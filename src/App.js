8a7f99ce3020802634a8657a2b806189d6009a8f
sst8872 <sst8872@gmail.com>
2024-07-12 01:39:01 +0900

    timer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const MyFlashcardComponent = () => {
           cards={flashcards}
           controls={true}
           showCount={true}
+          showBookMark={false}
           autoPlay={true}
+          timerDuration={3}
           onCardChange={(id, index) => console.log(`Card change detected: ID ${id}, Index: ${index}`)}
           onCardFlip={(id, index, state) => console.log(`Card flipped: ID ${id}, Index: ${index}, Flipped: ${state}`)}
 
